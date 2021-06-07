d3553ea1e709d48e4512d54685733c6c006061e7
Wing Chau <wing@devtography.com>
2021-06-07 15:35:38 +0100

    Setup jest & eslint

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+const { compilerOptions } = require('./tsconfig.json')
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = config = {
+  testEnvironment: 'node',
+  globals: {
+    'ts-test': {
+      tsconfig: 'tsconfig.json'
+    }
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  testMatch: [
+    '**/tests/**/*.test.(ts|js)'
+  ],
+  verbose: true,
+};
