de3c0da4a0ea38d59455f49a0667183220f8c987
Wing Chau <wing@devtography.com>
2021-06-10 17:30:11 +0100

    Refactor API /users
    - Included examples on usage of query & params.

--- tests/apis/users.test.ts
+++ tests/apis/users.test.ts
@@ -7,18 +7,32 @@ beforeAll(async () => {
   server = await createServer();
 });
 
-test('users', async () => {
-  const res = await server.inject().get('/users');
+describe('APIs - /users', () => {
+  it('should redirect to index', async () => {
+    const res = await server.inject().get('/users');
+    expect(res.statusCode).toBe(302);
+  });
 
-  expect(res.statusCode).toBe(200);
-  expect(res.body).toMatch('pong');
+  it('should return true on user id validation', async () => {
+    const res = await server.inject().get('/users').query({ id: 1 });
+    expect(res.statusCode).toBe(200);
+    expect(res.json()).toEqual({ user_id: 1, validated: true });
+  });
+
+  it('should return false on user id validation', async () => {
+    const res = await server.inject().get('/users').query({ id: 10 });
+    expect(res.statusCode).toBe(200);
+    expect(res.json()).toEqual({ user_id: 10, validated: false });
+  });
 });
 
-test('user by id', async () => {
-  const res = await server.inject().get('/users/1');
+describe('API - /users/:id', () => {
+  it('should return user id passed in', async () => {
+    const res = await server.inject().get('/users/1');
 
-  expect(res.statusCode).toBe(200);
-  expect(res.json()).toEqual({ user: 1 });
+    expect(res.statusCode).toBe(200);
+    expect(res.json()).toEqual({ user: 1 });
+  });
 });
 
 afterAll(async () => {
