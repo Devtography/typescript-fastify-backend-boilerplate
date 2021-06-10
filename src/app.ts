2faed4362cba33ebf8c060ca268b673d7ac3f0a2
Wing Chau <wing@devtography.com>
2021-06-10 09:35:19 +0100

    Setup fastify-cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import { fastify, FastifyInstance } from 'fastify';
+import fastifyCors from 'fastify-cors';
 
 async function build(opts = {}): Promise<FastifyInstance> {
   const app = fastify(opts);
+
+  // Registers plugins from Fastify ecosystem.
+  await app.register(fastifyCors);
 
   app.get('/', async (_req, _reply) => ({ hello: 'world' }));
 
