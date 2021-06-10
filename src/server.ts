c3acc374945825e25018bb5331ef3af82e99ff35
Wing Chau <wing@devtography.com>
2021-06-10 07:20:03 +0100

    Utilise NODE_ENV

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,20 @@
 import app from '_/app';
 
+const PRODUCTION = process.env.NODE_ENV === 'production';
+
+const PORT = PRODUCTION ? 80 : 3000;
+const LISTEN_ADDR = PRODUCTION ? '::' : '127.0.0.1';
+
 const server = app({
   logger: {
-    level: 'info',
-    prettyPrint: true,
+    level: PRODUCTION ? 'info' : 'debug',
+    prettyPrint: !PRODUCTION,
   },
 });
 
-server.listen(3000, (err, _address) => {
+server.listen(PORT, LISTEN_ADDR, (err, _address) => {
   if (err) {
-    // eslint-disable-next-line no-console
-    console.log(err);
+    server.log.error(err);
     process.exit(1);
   }
 });
