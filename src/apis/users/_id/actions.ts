f0c6d56fd58925a39fd4878d9873677c86afd0f4
Wing Chau <wing@devtography.com>
2021-06-10 16:33:24 +0100

    Fastify validation & serialisation
    - Implemented request validation & response serialisation.

--- src/apis/users/_id/actions.ts
+++ src/apis/users/_id/actions.ts
@@ -4,10 +4,28 @@ interface Params {
   id: number
 }
 
+// Request schema validation & response serialisation
+const opts = {
+  schema: {
+    params: {
+      type: 'object',
+      properties: {
+        id: { type: 'number' },
+      },
+    },
+  },
+  response: {
+    200: {
+      type: 'object',
+      user: 'number',
+    },
+  },
+};
+
 export default async (fastify: FastifyInstance): Promise<void> => {
-  fastify.get<{ Params: Params }>('/', async (req, reply) => {
+  fastify.get<{ Params: Params }>('/', opts, async (req, reply) => {
     const { id } = req.params;
 
-    await reply.send({ user: id });
+    await reply.code(200).send({ user: id });
   });
 };
--- tests/apis/users.test.ts
+++ tests/apis/users.test.ts
@@ -18,7 +18,7 @@ test('user by id', async () => {
   const res = await server.inject().get('/users/1');
 
   expect(res.statusCode).toBe(200);
-  expect(res.json()).toEqual({ user: '1' });
+  expect(res.json()).toEqual({ user: 1 });
 });
 
 afterAll(async () => {
